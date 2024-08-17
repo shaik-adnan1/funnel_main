a5e93e89f471cf47e5b2036736dd29f32a84b069
adnan_a <adnan_a@Adnans-MacBook-Pro.local>
2024-08-17 10:44:42 +0530

    reinitializing project

--- VgrowU_funnel/src/TextEffectPerWord.jsx
+++
@@ -1,9 +0,0 @@
-import { TextEffect } from "./components/TextEffect";
-
-export const TextEffectPerWord = () => {
-  return (
-    <TextEffect per='word' as='p' preset=''>
-     Test Text for hero section
-    </TextEffect>
-  );
-};
